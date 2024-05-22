e8193fbca0cc770488b8ce8a5ffa6bdde3f3c9a6
Mihail Marian <mihailmarian12@gmail.com>
2024-05-22 18:36:07 +0100

    add color utilities

--- src/utils/mappedColors.tsx
+++ src/utils/mappedColors.tsx
@@ -25,3 +25,47 @@ type ColoursToMap = Omit<
 >;
 
 let colorsToMap = copiedColors as ColoursToMap;
+
+export const hexToTailwind = (hex: string) => {
+  const normalizedHex = normalizeHex(hex);
+  const rgb = hexToRgb(normalizedHex);
+
+  // const inputLab = rgbToLab(hexToRgb(normalizeHex(hex)));
+
+  // const closestColor = labColors
+  //   .map((color) => ({
+  //     ...color,
+  //     deltaE: deltaE(inputLab, color.lab).toFixed(2),
+  //   }))
+  //   .sort((a, b) => a.deltaE - b.deltaE)[0];
+
+  // return {
+  //   tailwind: `${closestColor.main}-${closestColor.sub}`,
+  //   tailwindHex: closestColor.hex,
+  //   deltaE: closestColor.deltaE,
+  // };
+};
+
+export const normalizeHex = (hex: string) => {
+  let normalizedHex = hex;
+
+  if (normalizedHex.length == 3) {
+    normalizedHex = normalizedHex
+      .split("")
+      .map((x) => x + x)
+      .join("");
+  }
+
+  return "#" + normalizedHex;
+};
+
+export const hexToRgb = (hex: string) => {
+  // https://github.com/sindresorhus/hex-rgb/blob/main/index.js
+
+  const number = Number.parseInt(hex, 16);
+  const red = number >> 16;
+  const green = (number >> 8) & 255;
+  const blue = number & 255;
+
+  return { red, green, blue };
+};

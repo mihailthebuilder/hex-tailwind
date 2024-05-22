e976d673c5be29082bc1ff133d657ebcd2984101
Mihail Marian <mihailmarian12@gmail.com>
2024-05-22 21:21:28 +0100

    closestTailwindToHex first tests

--- src/utils/colors.test.ts
+++ src/utils/colors.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 
-import { hexToRgb, normalizeHex } from "./colors";
+import { closestTailwindToHex, hexToRgb, normalizeHex } from "./colors";
 
 test("normalizeHex with 3 characters", () => {
   expect(normalizeHex("FFF")).toBe("FFFFFF");
@@ -12,4 +12,17 @@ test("normalizeHex with 6 characters", () => {
 
 test("hexToRgb", () => {
   expect(hexToRgb("D6D3D1")).toStrictEqual({ red: 214, green: 211, blue: 209 });
+});
+
+test("closestTailwindToHex", () => {
+  const testCases = [
+    ["000000", "black"],
+    ["FFFFFF", "white"],
+  ];
+
+  testCases.forEach((testCase) => {
+    const [input, expected] = testCase;
+
+    expect(closestTailwindToHex(input as string)).toBe(expected as string);
+  });
 });

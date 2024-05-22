c7d13690865db5736de4fca08535aeda6e475967
Mihail Marian <mihailmarian12@gmail.com>
2024-05-22 18:19:41 +0100

    move tailwind color mapping to library

--- src/components/HexToTailwind.tsx
+++ src/components/HexToTailwind.tsx
@@ -1,31 +1,4 @@
 import { useState } from "react";
-import colors from "tailwindcss/colors";
-import type { DefaultColors } from "tailwindcss/types/generated/colors";
-
-let copiedColors = JSON.parse(JSON.stringify(colors));
-
-delete copiedColors.inherit;
-delete copiedColors.transparent;
-delete copiedColors.current;
-delete copiedColors.lightBlue;
-delete copiedColors.warmGray;
-delete copiedColors.trueGray;
-delete copiedColors.coolGray;
-delete copiedColors.blueGray;
-
-type ColoursToMap = Omit<
-  DefaultColors,
-  | "inherit"
-  | "transparent"
-  | "current"
-  | "lightBlue"
-  | "warmGray"
-  | "trueGray"
-  | "coolGray"
-  | "trueGray"
->;
-
-let coloursToMap = copiedColors as ColoursToMap;
 
 const HexToTailwind = () => {
   const [hexInput, setHexInput] = useState("");

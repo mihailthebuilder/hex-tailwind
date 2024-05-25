a88599c752e95e4cc9b5b37062a4beab5a689f6b
mmarian <mihailmarian12@gmail.com>
2024-05-25 22:22:51 +0100

    more form content

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -2,13 +2,37 @@ import { useState } from "react";
 
 const FeedbackForm = () => {
   const [feedback, setFeedback] = useState("");
-  const [loading, isLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setSubmitted(true);
+  };
+
+  if (submitted) {
+    return <p className="text-green-700 font-bold">Submitted!</p>;
+  }
+
   return (
-    <form>
-      <input type="text"></input>
-      <button type="submit"></button>
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <textarea
+        rows={3}
+        className="border border-black block rounded focus:ring-cyan-800 p-2 w-full"
+        value={feedback}
+        onChange={(event) => {
+          setFeedback(event.target.value);
+        }}
+        required
+      ></textarea>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-10 rounded"
+        disabled={isLoading}
+      >
+        {isLoading ? "..." : "Submit"}
+      </button>
     </form>
   );
 };

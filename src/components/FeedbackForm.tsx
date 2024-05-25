2f0c672ae3fcd4798249f331a34e97f06203c6f7
mmarian <mihailmarian12@gmail.com>
2024-05-25 22:31:56 +0100

    set up logic

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -4,11 +4,28 @@ const FeedbackForm = () => {
   const [feedback, setFeedback] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     setIsLoading(true);
-    setSubmitted(true);
+
+    fetch("https://formsubmit.co/ajax/mihailthebuilder@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: "New HEX to Tailwind submission",
+        message: { feedback },
+      }),
+    })
+      .then((response) => response.json())
+      .then(() => setSubmitted(true))
+      .catch((error) => {
+        setError(error);
+      });
   };
 
   if (submitted) {
@@ -17,6 +34,12 @@ const FeedbackForm = () => {
 
   return (
     <form className="space-y-5" onSubmit={handleSubmit}>
+      {error && (
+        <p className="text-red-700">
+          Something went wrong, error message {error}. Please reach out to me{" "}
+          <a href="https://linkedin.com/in/mihailmarian">on LinkedIn</a>
+        </p>
+      )}
       <textarea
         rows={3}
         className="border border-black block rounded focus:ring-cyan-800 p-2 w-full"
@@ -24,6 +47,7 @@ const FeedbackForm = () => {
         onChange={(event) => {
           setFeedback(event.target.value);
         }}
+        placeholder="Feed me feedback..."
         required
       ></textarea>
       <button
